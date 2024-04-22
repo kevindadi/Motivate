f1aeca985853039ac5ec074abe90786ac9242e45
zhangkw <zhangkw@tongji.edu.cn>
2024-04-22 15:29:01 +0800

    init

---
+++ message_error/src/main.rs
@@ -0,0 +1,23 @@
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    let (mut tx, mut rx) = mpsc::channel::<i32>(10);
+
+    let producer = tokio::spawn(async move {
+        for i in 1..=3 {
+            if let Err(_) = tx.send(i).await {
+                println!("Failed to send");
+            }
+        }
+    });
+
+    let consumer = tokio::spawn(async move {
+        if let Some(value) = rx.recv().await {
+            println!("Received: {}", value);
+            // 假设这里期待收到的是 3，但实际可能收到 1，因为它是第一个被发送的
+        }
+    });
+
+    let _ = tokio::join!(producer, consumer);
+}
